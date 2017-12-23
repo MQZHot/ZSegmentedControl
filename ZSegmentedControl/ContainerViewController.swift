943d04dafc0e51c0744c85aae8390791fc3d1c24
mqz <mqz1228@163.com>
2017-12-23 23:13:57 +0800

    normal

--- ZSegmentedControl/ContainerViewController.swift
+++ ZSegmentedControl/ContainerViewController.swift
@@ -26,17 +26,17 @@ class ContainerViewController: UIViewController {
         segmentedControl.delegate = self
         segmentedControl.backgroundColor = UIColor.lightGray
         segmentedControl.bounces = true
-        segmentedControl.selectedIndex = 8
+        segmentedControl.selectedIndex = 5
         
         segmentedControl.textColor = UIColor.yellow
         segmentedControl.textSelectedColor = UIColor.red
-        segmentedControl.setTitles(titles, adaptiveLeading: 10)
+//        segmentedControl.setTitles(titles, adaptiveLeading: 10)
         segmentedControl.sliderColor = UIColor.green
         
         let images = [#imageLiteral(resourceName: "sp1"), #imageLiteral(resourceName: "sp2"), #imageLiteral(resourceName: "sp3"), #imageLiteral(resourceName: "sp4"), #imageLiteral(resourceName: "sp5"), #imageLiteral(resourceName: "sp6"), #imageLiteral(resourceName: "sp7"), #imageLiteral(resourceName: "sp8")]
         let selectedImages = [#imageLiteral(resourceName: "p1"),#imageLiteral(resourceName: "p2"),#imageLiteral(resourceName: "p3"),#imageLiteral(resourceName: "p4"),#imageLiteral(resourceName: "p5"),#imageLiteral(resourceName: "p6"),#imageLiteral(resourceName: "p7"),#imageLiteral(resourceName: "p8")]
-//        segmentedControl.setImages(images, selectedImages: selectedImages, fixedWidth: 80)
-        segmentedControl.setHybridResource(titles, images: images, selectedImages: selectedImages, style: .normalWithSpace(0), fixedWidth: 80)
+        segmentedControl.setImages(images, selectedImages: selectedImages, fixedWidth: 80)
+//        segmentedControl.setHybridResource(titles, images: images, selectedImages: selectedImages, style: .normalWithSpace(0), fixedWidth: 80)
         view.addSubview(segmentedControl)
         
         scrollView.contentSize = CGSize(width: CGFloat(titles.count)*width, height:0)
